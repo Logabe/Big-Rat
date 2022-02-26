783ed1783be23650298966119f96e4a86f641631
Logan <logangbentley@gmail.com>
2022-02-27 08:57:23 +1300

    If this works I will be very happy!

--- src/Commands/hello.js
+++ src/Commands/hello.js
@@ -16,6 +16,6 @@ module.exports = {
     .setName("hello")
     .setDescription("Say Hello To me!"),
   execute: async (interaction, client) => {
-    return interaction.reply('Hi, ' + interaction.user.username + '!');
+    return interaction.reply('Ee, ' + getNameE(interaction.user.username) + '!');
   },
 };
