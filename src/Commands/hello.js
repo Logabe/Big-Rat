e6c39825d885dede1eb065b92ed77c7c55bdca06
Logan <logangbentley@gmail.com>
2022-02-27 07:31:42 +1300

    Changed hello message to be e's. Hope it works.

--- src/Commands/hello.js
+++ src/Commands/hello.js
@@ -1,10 +1,23 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
+const getName = (message) => {
+  if (message.guild) {
+    return message.member.displayName;
+  } else {
+    return message.author.username;
+  }
+};
+
+const getNameE = (message) => {
+  return getName(message).replace(/[a-zA-Z]/g, "e");
+};
+
+//Replace every letter in the senders name with the letter 'e'
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("hello")
-    .setDescription("Say Hello To me!"),
+    .setDescription("Say Hello to the rat!"),
   execute: async (interaction, client) => {
-    return interaction.reply("Choo choo! 🚅");
+    return interaction.reply("Eeeee, " + getNameE(interaction.message) +"!");
   },
 };
